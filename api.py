51c81a773145996a7132417b5890dae69c314b9f
MRE <mre@MREs-iMac.local>
2023-09-11 11:30:24 -0500

    Practica yahoo

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from yahoo_data_fetcher import get_price
+from flask import request
+
+from yahoo_data_fetcher import get_price, set_price
 
 
 app = Flask(__name__)
@@ -10,9 +12,26 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 
-@app.route("/api/")
-def api():
-    return get_price("DIS")
+@app.route("/api/<ticker>", methods=["GET"])
+def get_ticker(ticker):
+    return get_price(ticker)
+
+
+@app.route("/api/<ticker>", methods=["POST"])
+def post_ticker(ticker):
+    document = get_price(ticker)
+    return set_price(document)
+
+
+@app.route("/api/multiple/")
+def api_multiple():
+    tickers = request.args.get('tickers')
+    tickers = tickers.split(',')
+
+    result = []
+    for t in tickers:
+        result.append(get_price(t))
+    return result
 
 
 if __name__ == "__main__":
